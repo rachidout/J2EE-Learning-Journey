23433cdab2fb6b0235f5dd62a4f085e58a8a8ab3
rachidout <rachidoutsila7@gmail.com>
2026-02-27 15:19:01 +0000

    implimenting the ProduitModel

---
+++ produits/src/main/java/web/ProduitModele.java
@@ -0,0 +1,8 @@
+package web;
+import java.util.ArrayList;
+import java.util.List;
+import metier.entities.Produit;
+public class ProduitModele {
+   private String mc="";
+   private List<Produit> produits = new ArrayList<Produit>(); 
+}
